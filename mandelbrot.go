5e3e02d39490b33b0e824d2283519b6af550b66f
Brent Rowland <brent.rowland@ttiltd.com>
2019-06-05 23:04:33 -0700

    Calculation code

---
+++ mandelbrot.go
@@ -0,0 +1,105 @@
+package mandelbrot
+
+import "math"
+
+// Complex is our custom type to keep parity with other implementations.
+type Complex struct{ x, y float64 }
+
+func escapes(p Complex) bool {
+	return p.x*p.x+p.y*p.y > 4.0
+}
+
+func next(p Complex, p0 Complex) Complex {
+	return Complex{
+		x: p.x*p.x - p.y*p.y + p0.x,
+		y: 2.0*p.x*p.y + p0.y,
+	}
+}
+
+func iterations(p Complex, limit int) int {
+	p0 := p
+	for i := 0; i <= limit; i++ {
+		if escapes(p) {
+			return i
+		}
+		p = next(p, p0)
+	}
+	return limit
+}
+
+func iterArea(
+	w int, h int,
+	center Complex,
+	mag float64,
+	iterCallback func(i, x, y int, p Complex)) {
+	cx, cy := center.x, center.y
+	p := 4.0 / math.Min(float64(w), float64(h)) / mag
+	x0 := cx - float64(w)/2.0*p
+	y0 := cy + float64(h)/2.0*p
+	i := 0
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			iterCallback(i, x, y, Complex{x: x0 + float64(x)*p, y: y0 - float64(y)*p})
+			i++
+		}
+	}
+}
+
+// Color is an RGBA byte sequence.
+type Color [4]uint8
+
+// PALETTE is a set of colors stolen from another Mandelbrot set.
+var PALETTE = [16]Color{
+	{66, 30, 15, 255},
+	{25, 7, 26, 255},
+	{9, 1, 47, 255},
+	{4, 4, 73, 255},
+	{0, 7, 100, 255},
+	{12, 44, 138, 255},
+	{24, 82, 177, 255},
+	{57, 125, 209, 255},
+	{134, 181, 229, 255},
+	{211, 236, 248, 255},
+	{241, 233, 191, 255},
+	{248, 201, 95, 255},
+	{255, 170, 0, 255},
+	{204, 128, 0, 255},
+	{153, 87, 0, 255},
+	{106, 52, 3, 255},
+}
+
+// BLACK is used to represent the "lake."
+var BLACK = Color{0, 0, 0, 255}
+
+func color(v int, limit int) Color {
+	if v > 0 && v < limit {
+		return PALETTE[v%16]
+	}
+	return BLACK
+}
+
+func fillRGBA(
+	width int,
+	height int,
+	cx float64,
+	cy float64,
+	mag float64,
+	limit int) []uint8 {
+	bs := width * height * 4
+	img := make([]uint8, bs)
+	iterArea(
+		width,
+		height,
+		Complex{x: cx, y: cy},
+		mag,
+		func(i, x, y int, p Complex) {
+			v := iterations(p, limit)
+			pixel := color(v, limit)
+			offset := (i * 4)
+			for b := 0; b < 4; b++ {
+				img[offset+b] = pixel[b]
+			}
+		},
+	)
+	return img
+}
---
+++ mandelbrot_test.go
@@ -0,0 +1,108 @@
+package mandelbrot
+
+import (
+	"bytes"
+	"image"
+	"image/png"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIterations(t *testing.T) {
+	assert := assert.New(t)
+	p1 := Complex{x: 0.9, y: 0.9}
+	assert.Equal(iterations(p1, 10), 1)
+	p2 := Complex{x: 0.4, y: 0.4}
+	assert.Equal(iterations(p2, 10), 8)
+	p3 := Complex{x: 0.1, y: 0.01}
+	assert.Equal(iterations(p3, 10), 10)
+}
+
+func TestColor(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(color(0, 10), Color{0, 0, 0, 255})
+	assert.Equal(color(10, 10), Color{0, 0, 0, 255})
+	assert.Equal(color(4, 10), Color{0, 7, 100, 255})
+}
+
+func TestIterAreaCount(t *testing.T) {
+	assert := assert.New(t)
+	w := 6
+	h := 4
+	c := Complex{0.0, 0.0}
+	mag := 1.0
+	count := 0
+	iterArea(w, h, c, mag, func(i, x, y int, _point Complex) {
+		count++
+	})
+	assert.Equal(count, w*h)
+}
+
+func TestIterAreaRange(t *testing.T) {
+	assert := assert.New(t)
+	w := 6
+	h := 4
+	c := Complex{0.0, 0.0}
+	mag := 1.0
+	minX := 0.0
+	minY := 0.0
+	maxX := 0.0
+	maxY := 0.0
+	var img [24]Complex
+	iterArea(w, h, c, mag, func(i, _x, _y int, point Complex) {
+		img[i] = point
+		x, y := point.x, point.y
+		if x < minX {
+			minX = x
+		}
+		if x > maxX {
+			maxX = x
+		}
+		if y < minY {
+			minY = y
+		}
+		if y > maxY {
+			maxY = y
+		}
+	})
+	assert.Equal(img[0], Complex{x: -3.0, y: 2.0})
+	assert.Equal(img[(w*h-1)], Complex{x: 2.0, y: -1.0})
+	assert.Equal(minX, -3.0)
+	assert.Equal(maxX, 2.0)
+	assert.Equal(minY, -1.0)
+	assert.Equal(maxY, 2.0)
+}
+
+func TestFillRGBA(t *testing.T) {
+	assert := assert.New(t)
+	w := 6
+	h := 4
+	c := Complex{x: 0.0, y: 0.0}
+	mag := 1.0
+	limit := 1000
+
+	img := fillRGBA(w, h, c.x, c.y, mag, limit)
+	assert.Equal(img[3*4+0], uint8(25))
+	assert.Equal(img[3*4+1], uint8(7))
+	assert.Equal(img[3*4+2], uint8(26))
+	assert.Equal(img[3*4+3], uint8(255))
+}
+
+func TestEncodePNG(t *testing.T) {
+	w := 6
+	h := 4
+	c := Complex{x: 0.0, y: 0.0}
+	mag := 1.0
+	limit := 1000
+	img := fillRGBA(w, h, c.x, c.y, mag, limit)
+	pngImg := &image.NRGBA{
+		Pix:    img,
+		Stride: w * 4,
+		Rect:   image.Rect(0, 0, w, h),
+	}
+	var buf bytes.Buffer
+	if err := png.Encode(&buf, pngImg); err != nil {
+		t.Error(err)
+	}
+}
